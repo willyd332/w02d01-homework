076db3dbbe9200c39890aaedcde35fabf748bac1
WillyD <sopwill3@gmail.com>
2019-03-25 14:29:40 -0600

    data types

---
+++ js/app.js
@@ -0,0 +1,63 @@
+// MIXED DATA TYPES
+// 1. DATA TYPES
+/*
+lightSwitch = {
+  status: true,
+}
+
+users = {
+  user1: {
+    name: ,
+    address: email@email,
+  }
+}
+
+spaceship = {
+  hull: {
+    crew: [],
+    captain: ,
+},
+  lasers: ,
+  tractorBeam: ,
+  warpDrive: ,
+}
+
+students = {
+  student1: = {
+    name: ,
+    age: ,
+}
+  student2: = {
+    name: ,
+    age: ,
+  }
+}
+
+students = {
+  student1: = {
+    name: ,
+    age: ,
+    location: ,
+}
+  student2: = {
+    name: ,
+    age: ,
+    location: ,
+  }
+}
+
+students = {
+  student1: = {
+    name: ,
+    age: ,
+    location: ,
+    tvShows: [],
+}
+  student2: = {
+    name: ,
+    age: ,
+    location: ,
+    tvShows: [],
+  }
+}
+*/
